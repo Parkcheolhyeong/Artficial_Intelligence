b4ed419f72ee2bce3ab09d07a11ccaa46bbe7aef
parkch1124 <parkcheolhyeong@gmail.com>
2018-09-27 19:49:25 +0900

    added "Equal" rest "median"

--- resource.h
+++ resource.h
@@ -116,6 +116,8 @@
 #define ID_CHAP_THRESHOLDING            32873
 #define ID_CHAP_GLOBALTHRESHOLDING      32874
 #define ID_CHAP_HISTOGRAMSTRETCHING     32875
+#define ID_CHAP_MEANFILTERING           32876
+#define ID_CHAP_MEDIANFILTERING         32877
 
 // Next default values for new objects
 // 
@@ -123,7 +125,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
 #define _APS_NEXT_RESOURCE_VALUE        134
-#define _APS_NEXT_COMMAND_VALUE         32876
+#define _APS_NEXT_COMMAND_VALUE         32878
 #define _APS_NEXT_CONTROL_VALUE         1014
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
